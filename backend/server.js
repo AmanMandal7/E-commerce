3d292486da0e3e6f4aa3bccceef6a2b327ce6a02
AmanGH009 <aman.mandal.71334@gmail.com>
2022-10-28 12:34:32 +0530

    mongoDB connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDB = require('./config/database');
 
 // config
 dotenv.config({ path: "backend/config/config.env" });
+
+connectDB();
 
 app.listen(process.env.PORT, () => {
     console.log(`API is running on http://localhost:${process.env.PORT}`);
