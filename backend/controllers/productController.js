088c8fd3c12ef670a47587a3418f54be9957c03a
AmanGH009 <aman.mandal.71334@gmail.com>
2022-10-30 19:28:17 +0530

    API update and delete

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,9 +1,6 @@
 const Product = require('../models/productModel');
 
 
-
-
-
 // Create product -- Admin (only admin can access)
 exports.createProduct = async (req, res, next) => {
 
@@ -24,3 +21,49 @@ exports.getAllProducts = async (req, res) => {
         products
     })
 }
+
+
+// Update Product -- Admin
+exports.updateProduct = async (req, res, next) => {
+
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product not found!"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+
+// Delete Products
+exports.deleteProduct = async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        res.status(500).json({
+            success: false,
+            message: "Product not found!"
+        })
+    }
+
+    await product.remove();
+
+    res.status(200).json({
+        success: true,
+        message: "Product deleted successfully!"
+    });
+}
+
