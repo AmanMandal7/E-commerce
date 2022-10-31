9df987566b88895c3f1ba4ce69c35895df41bdd4
AmanGH009 <aman.mandal.71334@gmail.com>
2022-10-31 12:12:12 +0530

    Error Handling

---
+++ backend/middleware/error.js
@@ -0,0 +1,17 @@
+const ErrorHandler = require('../utils/errorhandler');
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error!";
+
+    // Wrong MongoDB Id error, this error is called CastError
+    if (err.name === "CastError") {
+        const message = `Resource not found. Invalid: ${err.path}`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    res.status(err.statusCode).json({
+        success: false,
+        message: err.message
+    });
+};
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,31 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
 
+
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exception`);
+    process.exit(1);
+});
+
+
+
 // config
 dotenv.config({ path: "backend/config/config.env" });
 
 connectDB();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`API is running on http://localhost:${process.env.PORT}`);
-})
+});
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`)
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+    server.close(() => {
+        process.exit(1);
+    });
+});
