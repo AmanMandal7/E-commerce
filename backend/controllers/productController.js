ab825b8e3dff85146817419ea4275790eedd78ce
AmanGH009 <aman.mandal.71334@gmail.com>
2022-11-01 15:52:33 +0530

    pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -19,12 +19,19 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 // Get All Products 
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
 
-    const apiFeature = new ApiFeatures(Product.find(), req.query).search().filter();
+    const resultPerPage = 5;
+    const productCount = await Product.countDocuments();
+
+    const apiFeature = new ApiFeatures(Product.find(), req.query)
+        .search()
+        .filter()
+        .pagination(resultPerPage);
     const products = await apiFeature.query;
 
     res.status(200).json({
         success: true,
         products,
+        productCount,
     });
 });
 
