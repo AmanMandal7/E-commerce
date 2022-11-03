bc695de945cb4f79aae1aabca2114ebef942ce25
AmanGH009 <aman.mandal.71334@gmail.com>
2022-11-03 13:57:58 +0530

    Authorization

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const errorMiddleware = require('./middleware/error');
+const cookieParser = require('cookie-parser');
 
 app.use(express.json());
+app.use(cookieParser());
 
 // Route imports
 const product = require('./routes/productRoute');
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
+const { isAuthenticatedUser } = require("../middleware/auth");
 
 
-router.route('/products').get(getAllProducts);
+router.route('/products').get(isAuthenticatedUser, getAllProducts);
 router.route('/product/new').post(createProduct);
 router.route('/product/:id').put(updateProduct).delete(deleteProduct).get(getProductDetails);
 
