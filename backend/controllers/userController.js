1cb90035d1c3f0c9918faacdd1602a9a83b1e6d6
AmanGH009 <aman.mandal.71334@gmail.com>
2022-11-09 18:49:41 +0530

    Update user role and delete user by admin only

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -205,3 +205,46 @@ exports.getSingleUser = catchAsyncErrors(async (req, res, next) => {
         user
     });
 });
+
+// Update User Role -- Admin
+exports.updateUserRole = catchAsyncErrors(async (req, res, next) => {
+
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role,
+    };
+
+    const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+
+    if (!user) {
+        return next(new ErrorHandler(`User does not exist with Id: ${req.params.id}`));
+    }
+
+    res.status(200).json({
+        success: true,
+        user
+    });
+});
+
+// Update User -- Admin
+exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+
+    const user = await User.findById(req.params.id);
+    // we will remove cloudinary
+
+    if (!user) {
+        return next(new ErrorHandler(`User does not exist with Id: ${req.params.id}`));
+    }
+
+    await user.remove();
+
+    res.status(200).json({
+        success: true,
+        message: "User deleted successfully!"
+    });
+});
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
-const { registerUser, loginUser, logoutUser, forgotPassword, resetPassword, getUserDetails, updatePassword, updateProfile, getAllUsers, getSingleUser } = require('../controllers/userController');
+const { registerUser, loginUser, logoutUser, forgotPassword, resetPassword, getUserDetails, updatePassword, updateProfile, getAllUsers, getSingleUser, updateUserRole, deleteUser } = require('../controllers/userController');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
@@ -13,6 +13,8 @@ router.route('/me').get(isAuthenticatedUser, getUserDetails);
 router.route('/password/update').put(isAuthenticatedUser, updatePassword);
 router.route('/me/update').put(isAuthenticatedUser, updateProfile);
 router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers);
-router.route('/admin/user/:id').get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser);
+router.route('/admin/user/:id').get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
+    .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole)
+    .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 module.exports = router;
