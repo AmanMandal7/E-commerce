df3d04b9cc86751998a61bf88bc200fc3cf6b674
AmanGH009 <aman.mandal.71334@gmail.com>
2022-11-11 19:41:35 +0530

    Order Controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,21 @@
+const Order = require('../models/orderModel');
+const Product = require('../models/productModel');
+const ErrorHandler = require('../utils/errorhandler');
+const catchAsyncErrors = require('../middleware/catchAsyncErrors');
+
+// Create new order
+exports.newOrder = catchAsyncErrors(async (req, res, next) => {
+
+    const { shippingInfo, orderItems, paymentInfo, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body;
+
+    const order = await Order.create({
+        shippingInfo, orderItems, paymentInfo, itemsPrice, taxPrice, shippingPrice, totalPrice,
+        paidAt: Date.now(),
+        user: req.user._id,
+    });
+
+    res.status(201).json({
+        success: true,
+        order
+    });
+});
