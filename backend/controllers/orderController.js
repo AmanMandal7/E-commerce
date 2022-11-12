0dd577d3514c62322161d498c0c180921ab7ff63
AmanGH009 <aman.mandal.71334@gmail.com>
2022-11-12 12:01:07 +0530

    Delete and Update order status By Admin

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -45,3 +45,75 @@ exports.myOrders = catchAsyncErrors(async (req, res, next) => {
         orders
     });
 });
+
+// Get all Orders -- Admin
+exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
+
+    const orders = await Order.find();
+
+    let totalAmount = 0;
+    orders.forEach((order) => {
+        totalAmount += order.totalPrice;
+    });
+
+    res.status(200).json({
+        success: true,
+        totalAmount,
+        orders,
+    });
+});
+
+//Update Order Status -- Admin
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+
+    const order = await Order.findById(req.params.id);
+
+    if (!order) {
+        return next(new ErrorHandler("Order not found with this id", 404));
+    };
+
+    if (order.orderStatus === "Delivered") {
+        return next(new ErrorHandler("You have already delivered this order", 400));
+    }
+
+    order.orderItems.forEach(async (o) => {
+        await updateStock(o.product, o.quantity);
+    });
+
+    order.orderStatus = req.body.status;
+
+    if (req.body.status === "Delivered") {
+        order.deliveredAt = Date.now();
+    };
+
+    await order.save({ validateBeforeSave: false });
+
+    res.status(200).json({
+        success: true,
+    });
+});
+
+async function updateStock(id, quantity) {
+    const product = await Product.findById(id);
+
+    product.Stock -= quantity;
+
+    await product.save({ validateBeforeSave: false });
+};
+
+
+// Delete order -- Admin
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+
+    const order = await Order.findById(req.params.id);
+
+    if (!order) {
+        return next(new ErrorHandler("Order not found with this id", 404));
+    }
+
+    await order.remove();
+
+    res.status(200).json({
+        success: true,
+    });
+});
